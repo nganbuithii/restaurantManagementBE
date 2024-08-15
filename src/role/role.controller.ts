657d97319c5fcebba2f53e916f096700e752d88c
Ngân <nganbui.23112003@gmail.com>
2024-08-16 04:55:40 +0700

    create role - [POST]

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RoleService } from './role.service';
+import { Role } from '@prisma/client';
+import { CreateRoleDto } from './dto/role.dto';
 
 @Controller('role')
-export class RoleController {}
+export class RoleController {
+    constructor(private readonly roleService: RoleService) { }
+    @Post()
+    create(@Body() body: CreateRoleDto): Promise<Role> {
+        return this.roleService.create(body);
+    }
+}
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Role, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
+import { CreateRoleDto } from './dto/role.dto';
 
 @Injectable()
 export class RoleService {
@@ -13,5 +15,15 @@ export class RoleService {
             update: {},
             create: { name: 'NORMAL_USER' },
         });
+    }
+
+    async create(body: CreateRoleDto): Promise<Role> {
+        const role = await this.prismaService.role.create({
+            data: {
+                name: body.name,
+            },
+        });
+    
+        return role;
     }
 }
